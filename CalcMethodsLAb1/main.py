8372f47152094295be35bc41a28642a74e64598e
RiabovAndrew <43186510+RiabovAndrew@users.noreply.github.com>
2020-10-07 15:24:55 +0300

    in positive direction

--- CalcMethodsLAb1/main.py
+++ CalcMethodsLAb1/main.py
@@ -12,7 +12,8 @@ class method_runge_kutta:
         print(self.h, self.x, self.y)
 
     def function(self, x, y):
-        return float(x*x + 3*y - 5)
+        # return float(1.2*pow(x, 3) + 3.1*x*y - 2.2)
+        return float(x + y)
 
     def fi_0(self, h):
         return h*self.function(self.x, self.y)
@@ -38,7 +39,7 @@ class method_runge_kutta:
         return (y_h2 - y_h)*pow(2, 3)/(pow(2, 3) - 1)
     
     def epsilon_h2(self):
-        # self.epsilon_h()/pow(2, 3)        // вариант намного лучше, но иногда мождет возникнуь ошибка из-за маленького числа
+        # self.epsilon_h()/pow(2, 3)        // вариант намного лучше, но мождет возникнуь ошибка из-за маленького числа
         tmp_y = self.y
         tmp_x = self.x
         self.y = self.y_i(self.h/2)
@@ -52,30 +53,30 @@ class method_runge_kutta:
     def delta(self):
         return self.b - self.x
 
-mrk = method_runge_kutta(1, 2)
+mrk = method_runge_kutta(0, 2)
 
 
 
 
-# while(mrk.x < mrk.b):
-#     if abs(mrk.epsilon_h2()) > mrk.epsilon:
-#         mrk.h = mrk.h / 2
-#         continue
-#     if abs(mrk.epsilon_h() <= mrk.epsilon):
-#         mrk.h = mrk.h * 2
-#         #print(mrk.h)
+while(mrk.x < mrk.b):
+    if abs(mrk.epsilon_h2()) > mrk.epsilon:
+        mrk.h = mrk.h / 2
+        continue
+    mrk.y = mrk.y_i(mrk.h)
+    mrk.x += mrk.h
+    print("f(", mrk.x, ") = ", mrk.y)
+    if abs(mrk.epsilon_h() <= mrk.epsilon):
+        mrk.h = mrk.h * 2
+        #print("step =", mrk.h)
+
+    if abs(mrk.delta()) < 0.000001:
+        break
+    if mrk.h > mrk.delta(): mrk.h = mrk.delta()
+
+# while(mrk.x <= mrk.b):
 #     mrk.y = mrk.y_i(mrk.h)
 #     print(mrk.y)
 #     mrk.x += mrk.h
-#     #print(mrk.x)
-#     if abs(mrk.delta()) < 0.000001:
-#         break
-#     if mrk.h > mrk.delta(): mrk.h = mrk.delta()
-
-while(mrk.x <= mrk.b):
-    mrk.y = mrk.y_i(mrk.h)
-    print(mrk.y)
-    mrk.x += mrk.h
 
 
 
