a4ade2a873aba2b61f0be9d546dfaaf2aeb24808
RiabovAndrew <43186510+RiabovAndrew@users.noreply.github.com>
2021-01-08 12:22:10 +0200

    Update main.py

--- CalcMethodsLAb1/main.py
+++ CalcMethodsLAb1/main.py
@@ -6,6 +6,7 @@ from config import y_0
 from config import h
 from config import to
 
+
 class accurate_method:
 
     def __init__(self, x_0, y_0, to, h):
@@ -27,51 +28,52 @@ class accurate_method:
         print(self.h, self.x, self.y)
 
     def function(self, x, y):
-        return float(1.2*pow(x, 3) + 3.1*x*y - 2.2)
+        return float(1.2 * pow(x, 3) + 3.1 * x * y - 2.2)
         # return float(0.9*x*y + 3.5*y - 2.1)
 
     def __fi_0(self, h):
-        return h*self.function(self.x, self.y)
+        return h * self.function(self.x, self.y)
 
     def __fi_1(self, h):
-        return h*self.function(self.combinate(self.x, h/2), self.combinate(self.y, self.__fi_0(h)/2))
+        return h * self.function(self.combinate(self.x, h / 2), self.combinate(self.y, self.__fi_0(h) / 2))
 
     def __fi_2(self, h):
-        return h*self.function(self.combinate(self.x, h/2), self.combinate(self.y, self.__fi_1(h)/2))
+        return h * self.function(self.combinate(self.x, h / 2), self.combinate(self.y, self.__fi_1(h) / 2))
 
     def __fi_3(self, h):
-        return h*self.function(self.combinate(self.x, h), self.combinate(self.y, self.__fi_2(h)))
+        return h * self.function(self.combinate(self.x, h), self.combinate(self.y, self.__fi_2(h)))
 
     def y_i(self, h):
-        return self.combinate(self.y, (self.__fi_0(h) + 2*self.__fi_1(h) + 2*self.__fi_2(h) + self.__fi_3(h))/6)
+        return self.combinate(self.y, (self.__fi_0(h) + 2 * self.__fi_1(h) + 2 * self.__fi_2(h) + self.__fi_3(h)) / 6)
 
     def epsilon_h(self):
         tmp_y = self.y
         tmp_x = self.x
-        self.y = self.y_i(self.h/2)
-        self.x = self.combinate(self.x, self.h/2)
-        y_h2 = self.y_i(self.h/2)
+        self.y = self.y_i(self.h / 2)
+        self.x = self.combinate(self.x, self.h / 2)
+        y_h2 = self.y_i(self.h / 2)
         self.y = tmp_y
         self.x = tmp_x
         y_h = self.y_i(self.h)
-        return (y_h2 - y_h)*pow(2, 3)/(pow(2, 3) - 1)
+        return (y_h2 - y_h) * pow(2, 3) / (pow(2, 3) - 1)
 
     def epsilon_h2(self):
         tmp_y = self.y
         tmp_x = self.x
-        self.y = self.y_i(self.h/2)
-        self.x = self.combinate(self.x, self.h/2)
-        y_h2 = self.y_i(self.h/2)
+        self.y = self.y_i(self.h / 2)
+        self.x = self.combinate(self.x, self.h / 2)
+        y_h2 = self.y_i(self.h / 2)
         self.y = tmp_y
         self.x = tmp_x
         y_h = self.y_i(self.h)
-        return (y_h2 - y_h)/(pow(2, 3) - 1)
+        return (y_h2 - y_h) / (pow(2, 3) - 1)
 
     def delta(self):
         if self.to < self.x:
             return self.x - self.to
         else:
             return self.to - self.x
+
 
 class method_runge_kutta:
 
@@ -94,42 +96,43 @@ class method_runge_kutta:
         print(self.h, self.x, self.y)
 
     def function(self, x, y):
-        return float(1.2*pow(x, 3) + 3.1*x*y - 2.2)
+        return float(1.2 * pow(x, 3) + 3.1 * x * y - 2.2)
         # return float(0.9*x*y + 3.5*y - 2.1)
 
     def __fi_0(self, h):
-        return h*self.function(self.x, self.y)
+        return h * self.function(self.x, self.y)
 
     def __fi_1(self, h):
-        return h*self.function(self.combinate(self.x, h/2), self.combinate(self.y, self.__fi_0(h)/2))
+        return h * self.function(self.combinate(self.x, h / 2), self.combinate(self.y, self.__fi_0(h) / 2))
 
     def __fi_2(self, h):
-        return h*self.function(self.combinate(self.x, h), self.combinate(self.y, -self.__fi_0(h) + 2*self.__fi_1(h)))
+        return h * self.function(self.combinate(self.x, h),
+                                 self.combinate(self.y, -self.__fi_0(h) + 2 * self.__fi_1(h)))
 
     def y_i(self, h):
-        return self.combinate(self.y, (self.__fi_0(h) + 4*self.__fi_1(h) + self.__fi_2(h))/6)
+        return self.combinate(self.y, (self.__fi_0(h) + 4 * self.__fi_1(h) + self.__fi_2(h)) / 6)
 
     def epsilon_h(self):
         tmp_y = self.y
         tmp_x = self.x
-        self.y = self.y_i(self.h/2)
-        self.x = self.combinate(self.x, self.h/2)
-        y_h2 = self.y_i(self.h/2)
+        self.y = self.y_i(self.h / 2)
+        self.x = self.combinate(self.x, self.h / 2)
+        y_h2 = self.y_i(self.h / 2)
         self.y = tmp_y
         self.x = tmp_x
         y_h = self.y_i(self.h)
-        return (y_h2 - y_h)*pow(2, 3)/(pow(2, 3) - 1)
+        return (y_h2 - y_h) * pow(2, 3) / (pow(2, 3) - 1)
 
     def epsilon_h2(self):
         tmp_y = self.y
         tmp_x = self.x
-        self.y = self.y_i(self.h/2)
-        self.x = self.combinate(self.x, self.h/2)
-        y_h2 = self.y_i(self.h/2)
+        self.y = self.y_i(self.h / 2)
+        self.x = self.combinate(self.x, self.h / 2)
+        y_h2 = self.y_i(self.h / 2)
         self.y = tmp_y
         self.x = tmp_x
         y_h = self.y_i(self.h)
-        return (y_h2 - y_h)/(pow(2, 3) - 1)
+        return (y_h2 - y_h) / (pow(2, 3) - 1)
 
     def delta(self):
         if self.to < self.x:
@@ -147,6 +150,7 @@ class drawing(method_runge_kutta):
         self.Y_F = []
         self.X_A = []
         self.Y_A = []
+
 
 class table:
 
@@ -169,7 +173,8 @@ drawing = drawing()
 tbl = PrettyTable()
 
 tbl.field_names = ["\033[36mX\033[0m", "\033[36mШаг\033[0m", "\033[36mНеточный Y\033[0m"]
-tbl.add_row(["\033[33m" + str(mrk.x) + "\033[0m", "\033[33m" + str(mrk.h) + "\033[0m", "\033[33m" + str(mrk.y) + "\033[0m"])
+tbl.add_row(
+    ["\033[33m" + str(mrk.x) + "\033[0m", "\033[33m" + str(mrk.h) + "\033[0m", "\033[33m" + str(mrk.y) + "\033[0m"])
 
 drawing.Y_I.append(mrk.y)
 drawing.X_I.append(mrk.x)
@@ -203,22 +208,21 @@ while am.delta() >= 0.000001:
     if abs(am.epsilon_h() <= am.epsilon):
         am.h = am.h * 2
 
-
-
 i = 0
 for element in table.X_I:
-    tbl.add_row(["\033[33m" + str(element) + "\033[0m", "\033[33m" + str(table.Step_I[i]) + "\033[0m", "\033[33m" + str(table.Y_I[i]) + "\033[0m"])
+    tbl.add_row(["\033[33m" + str(element) + "\033[0m", "\033[33m" + str(table.Step_I[i]) + "\033[0m",
+                 "\033[33m" + str(table.Y_I[i]) + "\033[0m"])
     i += 1
 print(tbl)
 tbl.clear_rows()
 
-
-
 tbl.field_names = ["\033[36mX\033[0m", "\033[36mШаг\033[0m", "\033[36mНеточный Y\033[0m"]
-tbl.add_row(["\033[33m" + str(x_0()) + "\033[0m", "\033[33m" + str(h()) + "\033[0m", "\033[33m" + str(y_0()) + "\033[0m"])
+tbl.add_row(
+    ["\033[33m" + str(x_0()) + "\033[0m", "\033[33m" + str(h()) + "\033[0m", "\033[33m" + str(y_0()) + "\033[0m"])
 i = 0
 for element in table.X_A:
-    tbl.add_row(["\033[33m" + str(element) + "\033[0m", "\033[33m" + str(table.Step_A[i]) + "\033[0m", "\033[33m" + str(table.Y_A[i]) + "\033[0m"])
+    tbl.add_row(["\033[33m" + str(element) + "\033[0m", "\033[33m" + str(table.Step_A[i]) + "\033[0m",
+                 "\033[33m" + str(table.Y_A[i]) + "\033[0m"])
     i += 1
 print(tbl)
 tbl = PrettyTable()
@@ -233,12 +237,17 @@ while (am.delta() >= 0.000001):
     table.Step_A.append(am.h)
     table.Y_A.append(am.y)
 
-tbl.field_names = ["\033[36mX\033[0m", "\033[36mШаг\033[0m", "\033[36mНеточный Y\033[0m", "\033[36mТочный Y\033[0m", "\033[36mПогрешность\033[0m"]
-tbl.add_row(["\033[33m" + str(x_0()) + "\033[0m", "\033[33m" + str(h()) + "\033[0m", "\033[33m" + str(y_0()) + "\033[0m", "\033[33m" + str(y_0()) + "\033[0m", "\033[33m" + str(0) + "\033[0m"])
+tbl.field_names = ["\033[36mX\033[0m", "\033[36mШаг\033[0m", "\033[36mНеточный Y\033[0m", "\033[36mТочный Y\033[0m",
+                   "\033[36mПогрешность\033[0m"]
+tbl.add_row(
+    ["\033[33m" + str(x_0()) + "\033[0m", "\033[33m" + str(h()) + "\033[0m", "\033[33m" + str(y_0()) + "\033[0m",
+     "\033[33m" + str(y_0()) + "\033[0m", "\033[33m" + str(0) + "\033[0m"])
 for element in table.X_I:
     i = table.X_A.index(element)
     i2 = table.X_I.index(element)
-    tbl.add_row(["\033[33m" + str(element) + "\033[0m", "\033[33m" + str(table.Step_I[i2]) + "\033[0m", "\033[33m" + str(table.Y_I[i2]) + "\033[0m", "\033[33m" + str(table.Y_A[i]) + "\033[0m", "\033[33m" + str(abs(table.Y_I[i2] - table.Y_A[i])) + "\033[0m"])
+    tbl.add_row(["\033[33m" + str(element) + "\033[0m", "\033[33m" + str(table.Step_I[i2]) + "\033[0m",
+                 "\033[33m" + str(table.Y_I[i2]) + "\033[0m", "\033[33m" + str(table.Y_A[i]) + "\033[0m",
+                 "\033[33m" + str(abs(table.Y_I[i2] - table.Y_A[i])) + "\033[0m"])
 print(tbl)
 tbl.clear_rows()
 tbl = PrettyTable()
@@ -268,12 +277,13 @@ while (mrk_fixed.delta() >= 0.000001):
     table.Step_I.append(mrk_fixed.h)
     table.Y_I.append(mrk_fixed.y)
 
-
 tbl.field_names = ["\033[36mX\033[0m", "\033[36mШаг\033[0m", "\033[36mНеточный Y\033[0m"]
-tbl.add_row(["\033[33m" + str(x_0()) + "\033[0m", "\033[33m" + str(0.2) + "\033[0m", "\033[33m" + str(y_0()) + "\033[0m"])
+tbl.add_row(
+    ["\033[33m" + str(x_0()) + "\033[0m", "\033[33m" + str(0.2) + "\033[0m", "\033[33m" + str(y_0()) + "\033[0m"])
 i = 0
 for element in table.X_I:
-    tbl.add_row(["\033[33m" + str(element) + "\033[0m", "\033[33m" + str(table.Step_I[i]) + "\033[0m", "\033[33m" + str(table.Y_I[i]) + "\033[0m"])
+    tbl.add_row(["\033[33m" + str(element) + "\033[0m", "\033[33m" + str(table.Step_I[i]) + "\033[0m",
+                 "\033[33m" + str(table.Y_I[i]) + "\033[0m"])
     i += 1
 print(tbl)
 
